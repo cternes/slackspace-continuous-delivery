c92ae509b11a7f6326ae7fa0ac2684345d85632f
cternes <github@slackspace.de>
2015-03-30 17:57:26 +0200

    Added jenkins

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,3 +7,11 @@ sed -i '$acomplete -F _docker docker' /etc/bash_completion.d/docker.io
 # Install java
 apt-get install -y openjdk-7-jdk git-core
 
+# Install jenkins
+useradd jenkins -s /bin/bash -m -G docker
+passwd -l jenkins
+cd /home/jenkins
+sudo -Hu jenkins git config --global user.name Jenkins
+sudo -Hu jenkins git config --global user.email jenkins@$(hostname)
+wget -q -O - http://mirrors.jenkins-ci.org/war-stable/latest/jenkins.war > /home/jenkins/jenkins.war
+
