446f8d82a3731de76a6ae6da87db67e8f78553f3
cternes <github@slackspace.de>
2015-03-30 17:49:23 +0200

    Added java

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,3 +4,6 @@ apt-get install -y docker.io
 ln -sf /usr/bin/docker.io /usr/local/bin/docker
 sed -i '$acomplete -F _docker docker' /etc/bash_completion.d/docker.io
 
+# Install java
+apt-get install -y openjdk-7-jdk git-core
+
